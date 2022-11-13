381b146a67dadae8b709762e03100a42853500c5
Mark Shliakhov <valmarksh@gmail.com>
2022-11-13 21:24:31 +0200

    task 2 finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,37 @@
 import Player from '@vimeo/player';
-let vaultKey = 'videoplayer-current-time';
-let memorySec = 0;
 const player = new Player('vimeo-player');
+let vaultKey = 'videoplayer-current-time';
+let memorySec;
+
+player.on('timeupdate', _.throttle(onPlay, 1000));
 
 function onPlay(data) {
-  memorySec = localStorage.setItem(vaultKey, data.seconds);
+  localStorage.setItem(vaultKey, JSON.stringify(data.seconds));
+  // memorySec = JSON.parse(localStorage.getItem(vaultKey));
+  // return memorySec;
+  // console.log(memorySec);
 }
-
-player.on('timeupdate', onPlay);
-
-player.on('play', function () {
-  console.log('played the video!');
-});
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
-player
-  .setCurrentTime(memorySec)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        'the time was less than 0 or greater than the video’s duration';
-        break;
+function getSecondsfromLS() {
+  memorySec = JSON.parse(localStorage.getItem(vaultKey));
+  player
+    .setCurrentTime(memorySec)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          'the time was less than 0 or greater than the video’s duration';
+          break;
 
-      default:
-        'player os broken';
-        break;
-    }
-  });
+        default:
+          'player іs broken';
+          break;
+      }
+    });
+}
+
+getSecondsfromLS();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+const formEl = document.querySelector('.feedback-form');
+const vaultKey = 'feedback-form-state';
+const userData = {};
 
+function fillContactFormElements() {
+  const userDataFromLS = JSON.parse(localStorage.getItem(vaultKey));
+
+  for (let prop in userDataFromLS) {
+    if (userDataFromLS.hasOwnProperty(prop)) {
+      formEl.elements[prop].value = userDataFromLS[prop];
+    }
+  }
+}
+
+if (localStorage.length !== 0) {
+  fillContactFormElements();
+}
+
+function onInputChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  userData[name] = value;
+
+  localStorage.setItem(vaultKey, JSON.stringify(userData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.target.reset();
+  console.log(userData);
+  localStorage.removeItem(vaultKey);
+}
+
+formEl.addEventListener('input', _.throttle(onInputChange, 500));
+formEl.addEventListener('submit', onFormSubmit);
